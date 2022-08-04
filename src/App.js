d0f7644ed7c88408f7c5f054b5d54d4e61dfa92f
hctrdubs24 <OtracuentadeHector24@outlook.com>
2022-08-03 20:44:56 -0500

    backend url added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState, useEffect } from "react";
 
 // const socket = io("http://localhost:5000");
-const socket = io('http://localhost:4000');
+const socket = io('https://mini-chat-backend.herokuapp.com/');
 
 
 function App() {
